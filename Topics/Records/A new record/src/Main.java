546fa149ea74e030ac8a96997b057c51c3a8c438
liuyunan <liuyunan.20@gmail.com>
2023-02-24 19:56:19 -0800

    Modify the record provided to have a name (String) and an address (String). Note that the order is important.
    Sample Input:
    Diamond
    Unknown
    
    Sample Output:
    Diamond has the address: Unknown

---
+++ Topics/Records/A new record/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//modify this Record
+record Person(String name, String address) {
+    public String name() {
+        return name;
+        }
+    public String address() {
+        return address;
+        }
+}
+
+//don't change the code below 
+class CheckRecord {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Person person = new Person(scanner.nextLine(), scanner.nextLine());
+        System.out.println(person.name() + " has the address: " + person.address());
+    }
+}
