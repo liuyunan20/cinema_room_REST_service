f1f2f564720a85380567687a5b684bb541e6ac81
liuyunan <liuyunan.20@gmail.com>
2023-07-05 20:10:15 -0700

    Write a service that takes a message and the number of its repetitions as constructor parameters and prints the message to the standard output the specified number of times.
    Use the provided template for your class.
    
    The testing system will start the service as a regular thread.
    
    Sample Input:
    Hello, Mary
    3
    
    Sample Output:
    Hello, Mary
    Hello, Mary
    Hello, Mary

---
+++ Topics/Custom threads/Message notifier as a thread/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class MessageNotifier extends Thread {
+    String msg;
+    int repeats;
+
+    public MessageNotifier(String msg, int repeats) {
+        this.msg = msg;
+        this.repeats = repeats;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < this.repeats; i++) {
+            System.out.println(this.msg);
+        }
+    }
+}
